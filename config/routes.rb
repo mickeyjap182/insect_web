7f1d564fa204c621f1d569f471b5af1eaec23434
mickeyjap182 <yoshitaka_8an9drums@msn.com>
2018-12-05 11:39:09 +0900

    Add a Static Pages for mulitplication

--- config/routes.rb
+++ config/routes.rb
@@ -1,4 +1,5 @@
 Rails.application.routes.draw do
+  get 'static_pages/multiplic'
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
     root 'application#top' # トップ
     get  '/multi', to: 'application#multiplication' # トップ
